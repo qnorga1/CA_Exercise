11e134cf3dc8f2df247c80b81c82f527cbca63a7
Quinten Norga <r0720478@stud-c8-19.esat.kuleuven.be>
2021-03-15 14:33:28 +0100

    test

---
+++ SIM/INCA_libs/irun.lnx8664.15.20.nc/.timestamp.ts
@@ -0,0 +1,13 @@
+1615811079 /users/students/r0720478/Documents/CA_Exercise/RTL/branch_unit.v
+1615811079 /users/students/r0720478/Documents/CA_Exercise/RTL/reg_arstn_en.v
+1615811079 /users/students/r0720478/Documents/CA_Exercise/RTL/cpu.v
+1615811079 /users/students/r0720478/Documents/CA_Exercise/RTL/reg_arstn.v
+1615811079 /users/students/r0720478/Documents/CA_Exercise/RTL/cpu_tb.v
+1615811079 /users/students/r0720478/Documents/CA_Exercise/RTL/register_file.v
+1615811079 /users/students/r0720478/Documents/CA_Exercise/RTL/mux_2.v
+1615811079 /users/students/r0720478/Documents/CA_Exercise/RTL/alu.v
+1615811079 /users/students/r0720478/Documents/CA_Exercise/RTL/saed32sram_mod.v
+1615811079 /users/students/r0720478/Documents/CA_Exercise/RTL/pc.v
+1615811079 /users/students/r0720478/Documents/CA_Exercise/RTL/sram.v
+1615811079 /users/students/r0720478/Documents/CA_Exercise/RTL/alu_control.v
+1615811079 /users/students/r0720478/Documents/CA_Exercise/RTL/control_unit.v
